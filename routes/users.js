acaa7336210ba1392a35949f287a66b8985c0c1a
adarsh0133 <adarsh90399@gmail.com>
2024-04-07 12:10:32 +0530

    db

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,23 @@
-var mongoose = require('mongoose');
-var passportLocalMongoose = require('passport-local-mongoose');
+var mongoose = require("mongoose");
+var passportLocalMongoose = require("passport-local-mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/codeenergia");
+mongoose
+  .connect(
+    "mongodb://127.0.0.1:27017/codeenergia" &&
+      "mongodb+srv://adarsh90399:jpajzRPLV3hTy2rz@codeenergia.kflnjth.mongodb.net/?retryWrites=true&w=majority&appName=codeenergia"
+  )
+  .then(() => {
+    console.log("connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 var userSchema = mongoose.Schema({
   username: String,
   password: String,
-  mobile: Number
-})
+  mobile: Number,
+});
 
 userSchema.plugin(passportLocalMongoose);
 
